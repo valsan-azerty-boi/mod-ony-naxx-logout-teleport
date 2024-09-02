408ed5c07c2c4a24fd326fb0a2f733695bca89bc
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2024-09-02 19:46:02 +0200

    add onyxia logout teleport

--- src/mod_ony_naxx_logout_teleport.cpp
+++ src/mod_ony_naxx_logout_teleport.cpp
@@ -14,6 +14,10 @@ public:
 		{
 			player->TeleportTo(0, 3082.641602f, -3725.781250f, 132.418884f, 0.002488f);
 		}
+		else if (player->GetMapId() == 249)
+		{
+			player->TeleportTo(1, -4737.995f, -3745.33f, 53.68f, 0.002488f);
+		}
 	}
 };
 
